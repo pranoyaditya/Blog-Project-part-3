ceceffa753d8c86510544eaaa6241228fd1b9520
pranoyaditya <pranoyaditya@gmail.com>
2024-12-06 14:00:41 +0600

    added login class view and updated url in urls.py inside author.

--- simple_blog_project/author/urls.py
+++ simple_blog_project/author/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', views.user_login, name='user_login'),
+    path('login/', views.UserLoginView.as_view(), name='user_login'),
     path('logout/', views.user_logout, name='user_logout'),
     path('profile/', views.profile, name='profile'),
     path('profile/edit_profile/', views.user_profile_update, name='profile_update'),
--- simple_blog_project/author/views.py
+++ simple_blog_project/author/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render,redirect
 from .forms import RegisterForm,UserLoginForm,ChangeUserDataForm,ChangePassWordForm
 from django.contrib.auth import authenticate,login,logout,update_session_auth_hash
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
 from posts.models import Post
 
 # Create your views here.
@@ -34,18 +36,42 @@ def user_login(request):
         form = UserLoginForm()
     return render(request, 'author/register.html', {'form' : form, 'type': 'Login'})
 
+# login class view.
+class UserLoginView(LoginView):
+    template_name = 'author/register.html'
+    
+    def get_success_url(self):
+        return reverse_lazy('profile')
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Logged in successfully')
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        messages.warning(self.request, 'Information was incorrect.')
+        return super().form_invalid(form)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['type'] = 'Login'
+        return context
+    
+    
+
+
+
 def user_logout(request):
     if request.user.is_authenticated:
         logout(request)
         return redirect('homePage')
     return redirect('user_login')
 
-@login_required
+
 def profile(request):
     data = Post.objects.filter(author = request.user)
     return render(request, 'author/profile.html', {'data' : data})
 
-@login_required
+
 def user_profile_update(request):
     form = ChangeUserDataForm(instance = request.user)
     if request.method == 'POST':
@@ -56,7 +82,7 @@ def user_profile_update(request):
             return redirect('profile')
     return render(request, 'author/update_profile.html', {'form' : form})
 
-@login_required
+
 def password_change(request):
     form = ChangePassWordForm(request.user)
     if request.method == 'POST':
