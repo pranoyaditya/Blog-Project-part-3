149604e136738defdfec96bc0182fc4f2aa56e3b
pranoyaditya <pranoyaditya@gmail.com>
2024-12-06 10:55:52 +0600

    updated the add post funtion to class view. Inherited LoginRequiredMixin which ensures a class to be executed when a user is logged in.

--- simple_blog_project/posts/views.py
+++ simple_blog_project/posts/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
-from .forms import PostForm
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView
+from .forms import PostForm
 from .models import Post
+
 
 # Create your views here.
 @login_required
@@ -15,6 +19,18 @@ def add_post(request):
     else:
         post_form = PostForm()
     return render(request, 'posts/postPage.html', {'form': post_form})
+
+# Add post using class based view.
+class AddPostCreateView(LoginRequiredMixin, CreateView):
+    model = Post # model to work with.
+    form_class = PostForm # form to work with.
+    template_name = 'posts/postPage.html' # which template to render.
+    success_url = reverse_lazy('homePage') # redirects to the url.
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user  
+        return super().form_valid(form)
+    
 
 @login_required
 def edit_post(request,id):
