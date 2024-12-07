72d6353a1b76f48e104a63becc4329545a7ccd36
pranoyaditya <pranoyaditya@gmail.com>
2024-12-07 12:07:47 +0600

    Fixed issues in comment model, DetailPostView, edited comment section in detailPostView and registered the comment model in admin.

--- simple_blog_project/posts/views.py
+++ simple_blog_project/posts/views.py
@@ -41,18 +41,20 @@ class DetailPostView(DetailView):
     template_name = 'posts/detailPostView.html'
     pk_url_kwarg = 'id'
 
+    def post(self, request, *args, **kwargs):
+        comment_form = CommentForm(data=self.request.POST)
+        post = self.get_object()
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+        return self.get(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         post = self.object 
-        comments = post.comments
-        if self.request.method == 'POST':
-            comment_form = CommentForm(data=self.request.POST)
-            if comment_form.is_valid():
-                new_comment = comment_form.save(commit=False)
-                new_comment.post = post
-                new_comment.save()
-        else:
-            comment_form = CommentForm()
+        comments = post.comments.all()
+        comment_form = CommentForm()
         
         context['comments'] = comments
         context['comment_form'] = comment_form
